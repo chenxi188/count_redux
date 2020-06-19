d81fb5d8eb622ef3f646fbe31bad5504891df1f2
chenxi <chenxifly2@qq.com>
2020-06-19 14:51:11 +0800

    containers/app.js的connet简写

--- src/containers/app.js
+++ src/containers/app.js
@@ -4,7 +4,12 @@ import {connect} from 'react-redux'
 import Counter from '../components/counter.js'
 import {increment,decrement} from '../redux/actions.js'
 
-const mapStateToProps=(state)=>({count:state})
-const mapDispatchToProps={increment,decrement}
+// const mapStateToProps=(state)=>({count:state})
+// const mapDispatchToProps={increment,decrement}
 
-export default connect(mapStateToProps,mapDispatchToProps)(Counter)
+// export default connect(mapStateToProps,mapDispatchToProps)(Counter)
+
+export default connect(
+	state=>({count:state}),
+	{increment,decrement},
+)(Counter)
